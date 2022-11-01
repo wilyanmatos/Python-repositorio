cab5cea166031b8d10ca72a2707f0f238f2f31cd
Wilyan <92753649+wilyanmatos@users.noreply.github.com>
2022-11-01 14:07:56 -0300

    Python Orientado a Objeto

---
+++ Python_OOP/pessoas.py
@@ -0,0 +1,4 @@
+class funcionario:
+    def __init__(self, nome, idade, salario):
+        print(f'\033[31m Salvo \033[m\nnome{nome}, idade{idade}, salario{salario}')
+    pass
---
+++ Python_OOP/principal.py
@@ -0,0 +1,12 @@
+from pessoas import funcionario
+
+nome = input('Nome: ')
+idade = int(input('Idade: '))
+salario = input('Salario: ')
+
+
+
+p1 = funcionario(nome=nome, idade=idade, salario=salario)
+
+
+
